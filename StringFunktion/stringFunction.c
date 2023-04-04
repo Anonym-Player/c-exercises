1f8c88f7606b5d334475ac4a14efe83120fda998
Nicolai Geisler <nicolai.geisler11@gmail.com>
2023-04-04 12:16:17 +0200

    Update ro exercise 7

--- StringFunktion/stringFunction.c
+++ StringFunktion/stringFunction.c
@@ -4,20 +4,16 @@
 #include <string.h>
 #include <ctype.h>
 
-void checkString(char word[]){
-	printf("\nLength: %ld ", strlen(word));
-	char min = word[0];
-	char max = word[0];
+int checkString(char word[], char *min, char *max){
 	for(int i = 0; i < strlen(word); i++){
-		if(word[i] <= min){
-			min = word[i];
+		if(word[i] <= *min){
+			*min = word[i];
 		}
-		if(word[i] >= max){
-			max = word[i];
+		if(word[i] >= *max){
+			*max = word[i];
 		}
 	}
-	printf("minChar: %c ", min);
-	printf("maxChar: %c\n", max);
+	return strlen(word);
 }
 
 int main(int arc, char *argv[]){
@@ -26,7 +22,13 @@ int main(int arc, char *argv[]){
 	while(1){
 		printf("\nPlease enter a string to check: ");
 		scanf(" %s", word);
-		checkString(word);
+		if(word[0] == 'q' && strlen(word) <= 1){
+			break;
+		}
+		char min = word[0];
+		char max = word[0];
+		printf("\nLength: %d, ", checkString(word, &min, &max));
+		printf("minChar: %c, maxChar: %c\n", min, max);
 	}
 	return 0;
 }
